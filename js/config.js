f31f3b8a2eac2b2317529165cdb120b41755b181
wsgfsm <49117341+wsgfsm@users.noreply.github.com>
2019-07-26 17:00:09 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+require.config({
+	baseUrl: '',  // 根目录，相对于HTML文件
+	paths: {
+		login: "./js/login",  // 相对于Js目录
+		tools: "./js/tools"
+	}	
+})
+
+require(['login']);
+
--- js/login.js
+++ js/login.js
@@ -1,10 +1,34 @@
-$(function() {
-    $("button:eq(0)").click(function() {
-       $.post("LoginServlet", {
-            name: $("input:eq(0)").val(),
-            pass: $("input:eq(1)").val()
-       }, function(data, textStatus) {
-            alert(data);
-       })
+define(['require', 'tools'], function(require) {
+    var tools = require('tools');
+
+    $(function() {
+        $("button:eq(0)").click(function() {
+            var data ={
+            	username: $("input:eq(0)").val(),
+            	password: $("input:eq(1)").val()
+            };
+            if(!tools.verificat.isNotNullTrim(data.username) || !tools.verificat.isNotNullTrim(data.password)) {
+                alert("输入不能为空！");          
+            } else {
+                $.ajax({
+        			type: "POST",
+        			url: "http://www.cxkball.club:2333/user/login",
+        			dataType: "json",
+        			contentType: "application/json",
+        			data: JSON.stringify(data),
+        			success: function(data) {
+        				if (data.message == "account_not_found") {
+        					alert("该账户不存在！");
+        				} else if (data.message == "password_incorrect") {
+        					alert("密码输入错误！");
+        				} else if(data.message == "success") {
+        					alert("登录成功！");
+        				}
+        			} 
+        		})
+            }
+          
+        });
     });
+
 });
