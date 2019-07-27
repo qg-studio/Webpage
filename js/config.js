5c4076f73e93fe7d7146f4a1e991ef366d54d6cd
wsgfsm <1552660178@qq.com>
2019-07-27 09:42:17 +0800

    fourth commit

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 require.config({
 	baseUrl: '',  // 根目录，相对于HTML文件
 	paths: {
-		login: "./js/login",  // 相对于Js目录
-		tools: "./js/tools"
+		login: "../js/login",  // 相对于Js目录
+		tools: "../js/tools"
 	}	
 })
 
